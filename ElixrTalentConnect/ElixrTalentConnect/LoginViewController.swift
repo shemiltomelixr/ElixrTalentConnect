0e6ce528d171037a1e028fc88a667f0706948245
Shemil Tom <155147968+shemiltomelixr@users.noreply.github.com>
2024-01-10 12:25:17 +0530

    intiall commit

---
+++ ElixrTalentConnect/ElixrTalentConnect/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  ElixrTalentConnect
+//
+//  Created by Shemil Tom on 10/01/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
