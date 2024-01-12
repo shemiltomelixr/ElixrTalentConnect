5b8ed83985274d9243e6826a86d8298e25769473
Shemil Tom <155147968+shemiltomelixr@users.noreply.github.com>
2024-01-12 17:39:57 +0530

    addeded login page features

--- ElixrTalentConnect/ElixrTalentConnect/LoginViewController.swift
+++ ElixrTalentConnect/ElixrTalentConnect/LoginViewController.swift
@@ -9,28 +9,37 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
- 
+    @IBOutlet weak var elixrSymbolImage: UIImageView!
+    @IBOutlet weak var signinBottomView: UIView!
     
-    
+    @IBOutlet weak var userEmail: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        elixrSymbolImage.layer.cornerRadius = 20
+        signinBottomView.layer.shadowColor = UIColor.black.cgColor
+        signinBottomView.layer.shadowOpacity = 0.5
+        signinBottomView.layer.shadowRadius = 4
+        signinBottomView.layer.cornerRadius = 20
+        
+        setUserTextFieldLeftSystemImage(textField: userEmail, systemName: "envelope", tintColor: .orange)
+        setUserTextFieldLeftSystemImage(textField: userPassword, systemName: "lock", tintColor: .orange)
+        
        
        
 
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    func setUserTextFieldLeftSystemImage(textField: UITextField, systemName: String, tintColor: UIColor) {
+        let imageView = UIImageView(image: UIImage(systemName: systemName))
+        imageView.tintColor = tintColor
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: textField.frame.height))
+        paddingView.addSubview(imageView)
+        imageView.center = CGPoint(x: paddingView.bounds.midX, y: paddingView.bounds.midY)
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
+       }
+ 
 
 }
